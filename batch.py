89d930e6d8ab8526c4770e16fe144833fa269a43
arifhamed <arifhamed2002@gmail.com>
2023-01-05 00:25:22 +0800

    05-Jan-2023 00:25:20: update batch.py

--- batch.py
+++ batch.py
@@ -1,13 +1,13 @@
 import os, sys, subprocess
 
-if len(sys.argv) >= 2:
+# check if target dir specified
+if len(sys.argv) >= 2 and os.path.exists(sys.argv[1]):
 	target_dir = sys.argv[1]
 	for target_video in [x for x in os.listdir(target_dir) if ".mp4" == x[-4:]]:
 		subprocess.run(["python3", "main.py", "-i", f"{target_dir}/{target_video}", "-z", "3"], shell=False)
-	if len(sys.argv) >= 3:
-		if sys.argv[2] in ("rm","-rm"):
-			os.system(f"rm -rf {target_dir}/*.mp4")
+	if len(sys.argv) >= 3 and sys.argv[2] in ("rm","-rm"):
+		os.system(f"rm -rf {target_dir}/*.mp4")
 	os.system(f"notify-send \"ezgif-essentials\" \"Finished creating .gif from {target_dir}\" --urgency=critical")
 else:
-	print("No target directory specificed.")
+	print("No target directory specificed or target directory specified is not valid directory/does not exist.")
 
